cde00389ec3594e5fd16f9ad0d4670b5a86df105
HU KE <403392669@qq.com>
2025-04-14 11:01:34 +0800

    加载模型在服务端

--- src/composables/useAudioDownloader.ts
+++ src/composables/useAudioDownloader.ts
@@ -1,18 +1,11 @@
 import { onMounted, ref, watch, type ComputedRef, computed } from "vue";
-import { BASE_URL } from '../utils/constants'
 import { downloadAudio } from '../utils/index'
 
 type AudioDownloaderProps = {
   textList: ComputedRef<string[]>;
 }
 
-const loadModels = async() => {
-  await fetch(`${BASE_URL}/set_gpt_weights?weights_path=/root/GPT-SoVITS/GPT_weights_v2/huke-e5.ckpt`)
-  await fetch(`${BASE_URL}/set_sovits_weights?weights_path=/root/GPT-SoVITS/SoVITS_weights_v2/huke_e4_s36.pth`)
-}
-
 export const useAudioDownloader = ({textList}: AudioDownloaderProps) => {
-
   const audioIndexUrlMap = ref<{ [key: number]: string }>({})
   const audioUrls = ref<any>([])
   const ttsModelLoaded = ref(false)
@@ -33,7 +26,6 @@ export const useAudioDownloader = ({textList}: AudioDownloaderProps) => {
   })
 
   onMounted(async() => {
-    await loadModels()
     ttsModelLoaded.value = true
   })
 
@@ -47,7 +39,6 @@ export const useAudioDownloader = ({textList}: AudioDownloaderProps) => {
   }
 
   return {
-    // audioUrls: computed(() => audioUrls.value.filter((url: string) => url)),
     audioUrls,
     finished,
     reset,
